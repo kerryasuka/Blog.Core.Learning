c81d848bb9fc71088b00a72a0d2f3f34204c5432
kerryasuka_github <tianyichuan2010@hotmail.com>
2019-11-27 10:46:04 +0800

    Blog.Core.IRepository

---
+++ Blog.Core/Blog.Core.IRepository/IRoleModulePermissionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Core.IRepository.Base;
+using Blog.Core.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.IRepository
+{
+    public interface IRoleModulePermissionRepository : IBaseRepository<RoleModulePermission>
+    {
+        Task<List<RoleModulePermission>> WithChildrenModel();
+        Task<List<TestMuchTableResult>> QueryMuchTable();
+    }
+}
---
+++ Blog.Core/Blog.Core.IRepository/IUserRoleRepository.cs
@@ -0,0 +1,12 @@
+﻿using Blog.Core.IRepository.Base;
+using Blog.Core.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Core.IRepository
+{
+    public interface IUserRoleRepository : IBaseRepository<UserRole>
+    {
+    }
+}
